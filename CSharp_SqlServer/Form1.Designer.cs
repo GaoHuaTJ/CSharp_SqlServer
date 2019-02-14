42cb3d61d68200dde6732bd468c67989a8cc3528
qidaxiang66@icloud.com <qidaxiang66@icloud.com>
2019-02-14 20:01:50 +0800

    SqlServer的数据调用

--- CSharp_SqlServer/Form1.Designer.cs
+++ CSharp_SqlServer/Form1.Designer.cs
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGetData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("黑体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.RowTemplate.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(755, 314);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnGetData.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnGetData.Location = new System.Drawing.Point(77, 360);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(185, 55);
+            this.btnGetData.TabIndex = 1;
+            this.btnGetData.Text = "数据查询";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(811, 446);
+            this.Controls.Add(this.btnGetData);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnGetData;
     }
 }
 
--- CSharp_SqlServer/Form1.cs
+++ CSharp_SqlServer/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CSharp_SqlServer
 {
@@ -15,6 +16,44 @@ namespace CSharp_SqlServer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        /// <summary>
+        ///连接数据库返回表数据
+        /// </summary>
+        /// <returns></returns>
+        private DataTable GetMessage()
+        {
+            string ConnectStr = string.Format(@"Data Source=.;Initial Catalog=C#练习使用;User Id=sa;Password=123456;");//数据库连接字符串
+            string SqlStr = string.Format("SELECT * FROM dbo.data");//sql字符串
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(SqlStr,ConnectStr);//创建数据适配器
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+
+
+            return dataTable;
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = GetMessage();
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                //dataGridView1.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells; //使得datagridview的列宽自适应，调整后 ，datagridview的宽度无法与窗体的宽度适应
+                dataGridView1.RowHeadersVisible = false;//datagridview前面的空白部分去除
+                dataGridView1.Width = dataGridView1.Columns[0].HeaderCell.Size.Width * dataGridView1.Columns.Count;
+                //dataGridView1.Height=(int)2*(dataGridView1.Columns[0].HeaderCell.Size.Height * dataGridView1.Rows.Count);
+
+            }
+
         }
     }
 }
